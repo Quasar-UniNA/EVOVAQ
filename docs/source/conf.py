3fff47eb8238315e28dca577eb1e0a7f958c7642
Angela Chiatto <95528517+angelachiatto@users.noreply.github.com>
2023-11-20 23:16:03 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath(".."))
 
 # sys.path.append("..")
 # -- Project information -----------------------------------------------------
