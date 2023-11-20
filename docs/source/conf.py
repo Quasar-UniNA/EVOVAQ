3ed43895287d6ed81fb115e7d64d422355be2bac
Angela Chiatto <95528517+angelachiatto@users.noreply.github.com>
2023-11-20 23:08:16 +0100

    change path in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,9 +5,9 @@
 
 import sys
 import os
-# sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
-sys.path.append("..")
+# sys.path.append("..")
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
