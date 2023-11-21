0d287260a633f922c785da03f424f4795184726c
Angela Chiatto <95528517+angelachiatto@users.noreply.github.com>
2023-11-21 18:26:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
     name='evovaq',
     packages=['evovaq'],
-    version='1.0.12',
+    version='1.0.13',
     description='EVOlutionary algorithms toolbox for VAriational Quantum circuits',
     author='Angela Chiatto',
     long_description=readme,
