f75a49426173f8149ab225613fadfeb812dc4e66
Angela Chiatto <95528517+angelachiatto@users.noreply.github.com>
2026-05-08 12:48:34 +0200

    fixed bug GA

--- evovaq/GeneticAlgorithm.py
+++ evovaq/GeneticAlgorithm.py
@@ -190,26 +190,20 @@ class GA(object):
             if max_nfev is not None and tot_nfev >= max_nfev:
                 pbar.close()
                 break
-
-            best, best_fit = sel_best(population, fitness, 1)
+            if self.elitism:
+                best, best_fit = sel_best(population, fitness, 1)
 
             # Evolve the population based on the genetic operators chosen
             offspring, fit_offspring, nfev = self.evolve_population(problem, population, fitness)
             tot_nfev += nfev
 
             if self.elitism:
-                joint_pop = np.concatenate((offspring, best))
-                joint_fitness = np.concatenate((fit_offspring, best_fit))
+                worst_idx = np.argmax(fit_offspring)
+                offspring[worst_idx] = best
+                fit_offspring[worst_idx] = best_fit
 
-                # Sort from the best (min fitness) to worst (max fitness) individual
-                sorted_idx = np.argsort(joint_fitness)
-
-                # Replacement
-                population[:] = joint_pop[sorted_idx[:pop_size]]
-                fitness[:] = joint_fitness[sorted_idx[:pop_size]]
-            else:
-                population[:] = offspring
-                fitness[:] = fit_offspring
+            population[:] = offspring
+            fitness[:] = fit_offspring
 
             # Store the best solution ever found
             best_tracker.update(population, fitness)
